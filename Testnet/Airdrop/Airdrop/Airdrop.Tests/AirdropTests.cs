4cbde033fd971e3f309cbbd52fab0f5c49bf16d8
Tyler Pena <tylerpena@me.com>
2019-08-09 22:16:22 -0700

    Airdrop initial commit

---
+++ Testnet/Airdrop/Airdrop/Airdrop.Tests/AirdropTests.cs
@@ -0,0 +1,128 @@
+using System;
+using Moq;
+using Stratis.SmartContracts.Networks;
+using Stratis.SmartContracts.CLR;
+using Xunit;
+
+namespace Stratis.SmartContracts.Samples.Tests
+{
+    public class AirdropTests
+    {
+        private readonly Mock<ISmartContractState> mockContractState;
+        private readonly Mock<IPersistentState> mockPersistentState;
+        private readonly Mock<IContractLogger> mockContractLogger;
+        private Address owner;
+        private Address sender;
+        private Address contract;
+        private Address spender;
+        private Address destination;
+        private ulong totalSupply;
+        private ulong endBlock;
+
+        public AirdropTests()
+        {
+            this.mockContractLogger = new Mock<IContractLogger>();
+            this.mockPersistentState = new Mock<IPersistentState>();
+            this.mockContractState = new Mock<ISmartContractState>();
+            this.mockContractState.Setup(s => s.PersistentState).Returns(this.mockPersistentState.Object);
+            this.mockContractState.Setup(s => s.ContractLogger).Returns(this.mockContractLogger.Object);
+            this.owner = "0x0000000000000000000000000000000000000001".HexToAddress();
+            this.sender = "0x0000000000000000000000000000000000000002".HexToAddress();
+            this.contract = "0x0000000000000000000000000000000000000003".HexToAddress();
+            this.spender = "0x0000000000000000000000000000000000000004".HexToAddress();
+            this.destination = "0x0000000000000000000000000000000000000005".HexToAddress();
+            this.totalSupply = 100_000;
+            this.endBlock = 1_000_000;
+        }
+
+        [Fact]
+        public void ConstructorSetsTotalSupplyNameSymbolAndEndBlock()
+        {
+            this.mockContractState.Setup(m => m.Message).Returns(new Message(this.contract, this.owner, 0));
+
+            // Initialize the smart contract set constructor props
+            var airdrop = new Airdrop(this.mockContractState.Object, this.totalSupply, this.destination, this.endBlock);
+
+            // Verify that PersistentState was called with the total supply, name, symbol and endblock
+            this.mockPersistentState.Verify(s => s.SetUInt64(nameof(Airdrop.TotalSupply), this.totalSupply));
+            this.mockPersistentState.Verify(s => s.SetUInt64(nameof(Airdrop.EndBlock), this.endBlock));
+            this.mockPersistentState.Verify(s => s.SetAddress(nameof(Airdrop.TokenContractAddress), this.destination));
+        }
+
+        [Fact]
+        public void SignUp()
+        {
+            this.mockContractState.Setup(m => m.Message).Returns(new Message(this.contract, this.owner, 0));
+            this.mockContractState.Setup(b => b.Block.Number).Returns(1);
+            this.mockPersistentState.Setup(e => e.GetUInt64("EndBlock")).Returns(this.endBlock);
+
+            // Initialize the smart contract set constructor props
+            var airdrop = new Airdrop(this.mockContractState.Object, this.totalSupply, this.destination, this.endBlock);
+
+            airdrop.SignUp();
+
+            this.mockPersistentState.Verify(s => s.SetStruct($"Status:{this.owner}", Airdrop.Status.APPROVED));
+        }
+
+        [Fact]
+        public void UnsetAccountReturnsNull()
+        {
+            this.mockContractState.Setup(m => m.Message).Returns(new Message(this.contract, this.owner, 0));
+
+            // Initialize the smart contract set constructor props
+            var airdrop = new Airdrop(this.mockContractState.Object, this.totalSupply, this.destination, this.endBlock);
+
+            var status = airdrop.GetAccountStatus(this.owner);
+
+            Assert.Equal(Airdrop.Status.UNAPPROVED, status);
+        }
+
+        [Fact]
+        public void SignUpSetsStatusToAPPROVED()
+        {
+            this.mockContractState.Setup(m => m.Message).Returns(new Message(this.contract, this.owner, 0));
+            this.mockContractState.Setup(b => b.Block.Number).Returns(1);
+            this.mockPersistentState.Setup(e => e.GetUInt64("EndBlock")).Returns(this.endBlock);
+
+            // Initialize the smart contract set constructor props
+            var airdrop = new Airdrop(this.mockContractState.Object, this.totalSupply, this.destination, this.endBlock);
+
+            var status = airdrop.SignUp();
+
+            // Assert status succeeds
+            Assert.True(status);
+
+            // Verfies that this was run during SignUp
+            this.mockPersistentState.Verify(s => s.SetStruct($"Status:{this.owner}", Airdrop.Status.APPROVED));
+
+            // Set the status manually in persistent state
+            this.mockPersistentState.Setup(s => s.GetStruct<Airdrop.Status>($"Status:{this.owner}")).Returns(Airdrop.Status.APPROVED);
+
+            // Getting the status returns APPROVED
+            Assert.Equal(Airdrop.Status.APPROVED, airdrop.GetAccountStatus(this.owner));
+        }
+
+        [Fact]
+        public void DuplicateSignUpFails()
+        {
+            this.mockContractState.Setup(m => m.Message).Returns(new Message(this.contract, this.owner, 0));
+            this.mockContractState.Setup(b => b.Block.Number).Returns(1);
+            this.mockPersistentState.Setup(e => e.GetUInt64("EndBlock")).Returns(this.endBlock);
+
+            // Initialize the smart contract set constructor props
+            var airdrop = new Airdrop(this.mockContractState.Object, this.totalSupply, this.destination, this.endBlock);
+
+            var status = airdrop.SignUp();
+
+            // Assert status succeeds
+            Assert.True(status);
+
+            // Set the status manually in persistent state
+            this.mockPersistentState.Setup(s => s.GetStruct<Airdrop.Status>($"Status:{this.owner}")).Returns(Airdrop.Status.APPROVED);
+
+            status = airdrop.SignUp();
+
+            Assert.False(status);
+        }
+    }
+}
---
+++ Testnet/Airdrop/Airdrop/Airdrop/Airdrop.cs
@@ -0,0 +1,103 @@
+﻿using Stratis.SmartContracts;
+using Stratis.SmartContracts.Standards;
+using System.Linq;
+using System;
+
+public class Airdrop : SmartContract 
+{
+    public Airdrop(ISmartContractState smartContractState, ulong totalSupply, Address tokenContractAddress, ulong endBlock)
+        : base(smartContractState)
+    {
+        this.TotalSupply = totalSupply;
+        this.TokenContractAddress = tokenContractAddress;
+        this.EndBlock = endBlock;
+    }
+
+    public ulong TotalSupply
+    {
+        get => PersistentState.GetUInt64(nameof(this.TotalSupply));
+        private set => PersistentState.SetUInt64(nameof(this.TotalSupply), value);
+    }
+
+    public Address TokenContractAddress
+    {
+        get => PersistentState.GetAddress(nameof(this.TokenContractAddress));
+        private set => PersistentState.SetAddress(nameof(TokenContractAddress), value);
+    }
+
+    public ulong EndBlock
+    {
+        get => PersistentState.GetUInt64(nameof(this.EndBlock));
+        private set => PersistentState.SetUInt64(nameof(this.EndBlock), value);
+    }
+
+    private ulong Index
+    {
+        get => this.PersistentState.GetUInt64("Index");
+        set => PersistentState.SetUInt64("Index", value);
+    }
+
+    public Status GetAccountStatus(Address address)
+    {
+        return PersistentState.GetStruct<Status>($"Status:{address}");
+    }
+
+    private void SetAccountStatus(Address address, Status status)
+    {
+        PersistentState.SetStruct($"Status:{address}", status);
+    }
+
+    public bool SignUp()
+    {
+        if (this.Block.Number > this.EndBlock) return false;
+
+        if (GetAccountStatus(Message.Sender) != Status.UNAPPROVED) return false;
+
+        this.Index++;
+
+        SetAccountStatus(Message.Sender, Status.APPROVED);
+
+        Log(new StatusLog { Owner = Message.Sender, OldStatus = Status.UNAPPROVED, Status = Status.APPROVED });
+
+        return true;
+    }
+
+    public bool Withdraw()
+    {
+        if (this.Block.Number < this.EndBlock) return false;
+
+        if (GetAccountStatus(Message.Sender) != Status.APPROVED) return false;
+
+        ulong amount = (ulong)Math.Floor((decimal)this.TotalSupply / this.Index);
+
+        var result = Call(this.TokenContractAddress, 0, "TransferTo", new object[] {
+            new { address = Message.Sender},
+            new { amount }
+        });
+
+        if (!result.Success) return false;
+
+        SetAccountStatus(Message.Sender, Status.FUNDED);
+
+        Log(new StatusLog { Owner = Message.Sender, OldStatus = Status.APPROVED, Status = Status.FUNDED });
+
+        return true;
+    }
+
+    public struct StatusLog
+    {
+        [Index]
+        public Address Owner;
+
+        public Status OldStatus;
+
+        public Status Status;
+    }
+
+    public enum Status
+    {
+        UNAPPROVED = 0,
+        APPROVED = 1,
+        FUNDED = 2
+    }
+}
