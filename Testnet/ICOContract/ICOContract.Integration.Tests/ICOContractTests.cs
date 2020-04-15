f70b87b225ec76b20704c28ee7541029989d5670
YakupIpek <yakupipek@gmail.com>
2020-04-15 14:01:22 +0300

    Integration test for invest

--- Testnet/ICOContract/ICOContract.Integration.Tests/ICOContractTests.cs
+++ Testnet/ICOContract/ICOContract.Integration.Tests/ICOContractTests.cs
@@ -38,49 +38,56 @@ namespace ICOContract.Integration.Tests
             }
         }
 
-        //[Fact]
-        //public void Investment_Test()
-        //{
-        //    // Compile the contract we want to deploy
-        //    ContractCompilationResult compilationResult = ContractCompiler.CompileFile("ICOContract.cs");
-        //    Assert.True(compilationResult.Success);
-        //    using (var chain = new TestChain().Initialize())
-        //    {
-        //        // Get an address we can use for deploying
-        //        var deployerAddress = chain.PreloadedAddresses[0];
-        //        var totalSupply = 100ul;
-        //        var rate = 2ul;
+        [Fact]
+        public void Verify_Invest()
+        {
+            // Compile the contract we want to deploy
+            ContractCompilationResult compilationResult = ContractCompiler.CompileFile("ICOContract.cs");
+            Assert.True(compilationResult.Success);
+            using (var chain = new TestChain().Initialize())
+            {
+                // Get an address we can use for deploying
+                var owner = chain.PreloadedAddresses[0];
+                var totalSupply = 100ul;
+                var amount = 20ul;
+                var gasPrice = 100ul;
+                var fee = 0.01d;
+                var serializer = new Serializer(new ContractPrimitiveSerializer(new SmartContractsPoARegTest()));
 
-        //        var parameters = new object[] { totalSupply, "Gluon", "Glu", 1000ul /*duration*/, rate };
+                var periods = new SalePeriodInput[] { new SalePeriodInput { Multiplier = 2, DurationBlocks = 1 } };
+                var parameters = new object[] { totalSupply, "Gluon", "Glu", serializer.Serialize(periods) };
 
-        //        // Create and send transaction to mempool with parameters
-        //        var createResult = chain.SendCreateContractTransaction(deployerAddress, compilationResult.Compilation, 0, parameters);
+                var createResult = chain.SendCreateContractTransaction(owner, compilationResult.Compilation, 0, parameters);
 
-        //        // Mine a block which will contain our sent transaction
-        //        chain.MineBlocks(1);
+                chain.MineBlocks(1);
 
-        //        // Check the receipt to see that contract deployment was successful
-        //        var receipt = chain.GetReceipt(createResult.TransactionId);
-        //        Assert.Equal(deployerAddress, receipt.From);
+                var investor = chain.PreloadedAddresses[1];
 
-        //        // Check that the code is indeed saved on-chain
-        //        var savedCode = chain.GetCode(createResult.NewContractAddress);
-        //        Assert.NotNull(savedCode);
+                var currentBalance = chain.GetBalance(investor);
+                var investResult = chain.SendCallContractTransaction(investor, "Invest", createResult.NewContractAddress, amount, gasPrice: gasPrice, feeAmount: fee);
+                chain.MineBlocks(1);
 
-        //        // Use another identity to invest
-        //        var investorAddress = chain.PreloadedAddresses[1];
+                var receipt = chain.GetReceipt(investResult.TransactionId);
 
-        //        // Send a call to the invest method
-        //        var callResult = chain.SendCallContractTransaction(investorAddress, "Invest", createResult.NewContractAddress, 5ul);
-        //        chain.MineBlocks(1);
+                var localCallResult = chain.CallContractMethodLocally(owner, "TokenBalance", createResult.NewContractAddress, 0);
 
-        //        receipt = chain.GetReceipt(callResult.TransactionId);
+                Assert.Equal(60ul, (ulong)localCallResult.Return); // All tokens are sold
 
-        //        // Call a method locally to check the state is as expected
-        //        var localCallResult = chain.CallContractMethodLocally(investorAddress, "TokenBalance", createResult.NewContractAddress, 0);
-        //        Assert.Equal(90ul, (ulong)localCallResult.Return);
-        //    }
-        //}
+                var contractBalance = chain.GetBalance(createResult.NewContractAddress);
+
+                Assert.Equal(Money.Coins(20), contractBalance);
+
+                var investorTokenBalance = (ulong)chain.CallContractMethodLocally(owner, "GetBalance", createResult.NewContractAddress, 0, new object[] { investor }).Return;
+
+                // Verify investor's token balance
+                Assert.Equal(40ul, investorTokenBalance);
+
+                var transactionCost = Money.Satoshis(receipt.GasUsed * gasPrice) + Money.Coins((decimal)fee);
+                var spendAmount = currentBalance - chain.GetBalance(investor);
+
+                Assert.Equal(Money.Coins(20) + transactionCost, spendAmount);
+            }
+        }
 
         [Fact]
         public void OverSold_Investment_Test()
