ccbf417cb56d146825755289f8906dfefbcc6e4c
YakupIpek <yakupipek@gmail.com>
2020-06-15 15:52:25 +0300

    StandardToken code is inlined

--- Testnet/DividendToken/DividendToken/DividendToken.cs
+++ Testnet/DividendToken/DividendToken/DividendToken.cs
@@ -2,8 +2,9 @@
 using Stratis.SmartContracts.Standards;
 
 [Deploy]
-public class DividendToken : StandardToken
+public class DividendToken : SmartContract, IStandardToken
 {
+
     public ulong Dividends
     {
         get => PersistentState.GetUInt64(nameof(this.Dividends));
@@ -11,11 +12,17 @@ public class DividendToken : StandardToken
     }
 
     public Account GetAccount(Address address) => PersistentState.GetStruct<Account>($"Account:{address}");
+
     private void SetAccount(Address address, Account account) => PersistentState.SetStruct($"Account:{address}", account);
 
-    public DividendToken(ISmartContractState smartContractState, ulong totalSupply, string name, string symbol)
-        : base(smartContractState, totalSupply, name, symbol)
+
+    public DividendToken(ISmartContractState state, ulong totalSupply, string name, string symbol)
+        : base(state)
     {
+        this.TotalSupply = totalSupply;
+        this.Name = name;
+        this.Symbol = symbol;
+        this.SetBalance(Message.Sender, totalSupply);
     }
 
     /// <summary>
@@ -27,19 +34,19 @@ public class DividendToken : StandardToken
         Dividends += Message.Value;
     }
 
-    new public bool TransferTo(Address to, ulong amount)
+    public bool TransferTo(Address to, ulong amount)
     {
         UpdateAccount(Message.Sender);
 
-        return base.TransferTo(to, amount);
+        return TransferTokensTo(to, amount);
     }
 
-    new public bool TransferFrom(Address from, Address to, ulong amount)
+    public bool TransferFrom(Address from, Address to, ulong amount)
     {
         UpdateAccount(from);
         UpdateAccount(to);
 
-        return base.TransferFrom(from, to, amount);
+        return TransferTokensFrom(from, to, amount);
     }
 
     void UpdateAccount(Address address) => GetUpdatedAccount(address);
@@ -101,28 +108,8 @@ public class DividendToken : StandardToken
 
         public ulong CreditedDividends;
     }
-}
 
-/// <summary>
-/// Implementation of a standard token contract for the Stratis Platform.
-/// </summary>
-public class StandardToken : SmartContract, IStandardToken
-{
-    /// <summary>
-    /// Constructor used to create a new instance of the token. Assigns the total token supply to the creator of the contract.
-    /// </summary>
-    /// <param name="smartContractState">The execution state for the contract.</param>
-    /// <param name="totalSupply">The total token supply.</param>
-    /// <param name="name">The name of the token.</param>
-    /// <param name="symbol">The symbol used to identify the token.</param>
-    public StandardToken(ISmartContractState smartContractState, ulong totalSupply, string name, string symbol)
-        : base(smartContractState)
-    {
-        this.TotalSupply = totalSupply;
-        this.Name = name;
-        this.Symbol = symbol;
-        this.SetBalance(Message.Sender, totalSupply);
-    }
+    #region StandardToken code is inlined
 
     public string Symbol
     {
@@ -142,7 +129,6 @@ public class StandardToken : SmartContract, IStandardToken
         get => PersistentState.GetUInt64(nameof(this.TotalSupply));
         private set => PersistentState.SetUInt64(nameof(this.TotalSupply), value);
     }
-
     /// <inheritdoc />
     public ulong GetBalance(Address address)
     {
@@ -155,7 +141,7 @@ public class StandardToken : SmartContract, IStandardToken
     }
 
     /// <inheritdoc />
-    public bool TransferTo(Address to, ulong amount)
+    private bool TransferTokensTo(Address to, ulong amount)
     {
         if (amount == 0)
         {
@@ -181,7 +167,7 @@ public class StandardToken : SmartContract, IStandardToken
     }
 
     /// <inheritdoc />
-    public bool TransferFrom(Address from, Address to, ulong amount)
+    private bool TransferTokensFrom(Address from, Address to, ulong amount)
     {
         if (amount == 0)
         {
@@ -258,4 +244,5 @@ public class StandardToken : SmartContract, IStandardToken
 
         public ulong Amount;
     }
+    #endregion
 }
