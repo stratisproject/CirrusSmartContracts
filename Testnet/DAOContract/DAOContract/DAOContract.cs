81830b9fe01b56b042199ea1395866604e221793
YakupIpek <yakupipek@gmail.com>
2021-03-01 04:59:11 +0300

    Contract updates
    (cherry picked from commit ad565cb645051b3ddde211ddb46837ba5f9a016f)

--- Testnet/DAOContract/DAOContract/DAOContract.cs
+++ Testnet/DAOContract/DAOContract/DAOContract.cs
@@ -1,22 +1,157 @@
 ﻿using Stratis.SmartContracts;
+using System;
 
-namespace DAOContract
+[Deploy]
+public class DAOContract : SmartContract
 {
-    [Deploy]
-    public class DAOContract : SmartContract
+    /// <summary>
+    /// Min quorum for yes votes
+    /// </summary>
+    public uint MinQuorum
     {
-        public ulong Dividends
+        get => State.GetUInt32(nameof(MinQuorum));
+        private set => State.SetUInt32(nameof(MinQuorum), value);
+    }
+
+    public uint LastProposalIndex
+    {
+        get => State.GetUInt32(nameof(MinQuorum));
+        private set => State.SetUInt32(nameof(MinQuorum), value);
+    }
+
+    public bool IsWhitelisted(Address address) => State.GetBool($"Whitelisted:{address}");
+    public void SetWhitelisted(Address address, bool allowed) => State.SetBool($"Whitelisted:{address}", allowed);
+
+    public uint GetYesVotes(uint proposalId) => State.GetUInt32($"YesVotes:{proposalId}");
+    private void SetYesVotes(uint proposalId, uint value) => State.SetUInt32($"YesVotes:{proposalId}", value);
+
+    public uint GetNoVotes(uint proposalId) => State.GetUInt32($"NoVotes:{proposalId}");
+    private void SetNoVotes(uint proposalId, uint value) => State.SetUInt32($"NoVotes:{proposalId}", value);
+
+    public uint GetVote(uint proposalId, Address address) => State.GetUInt32($"Vote:{proposalId}:{address}");
+
+    public Proposal GetProposal(uint index) => State.GetStruct<Proposal>($"Proposals:{index}");
+
+    public void SetProposal(uint index, Proposal proposal) => State.SetStruct($"Proposals:{index}", proposal);
+
+    public DAOContract(ISmartContractState state, uint minQuorum)
+        : base(state)
+    {
+        this.MinQuorum = minQuorum;
+    }
+
+    public uint CreateProposal(Address recipent, ulong amount, uint debatingDuration, string description)
+    {
+        var proposal = new Proposal
         {
-            get => State.GetUInt64(nameof(Dividends));
-            private set => State.SetUInt64(nameof(Dividends), value);
+            Amount = amount,
+            Description = description,
+            Recipient = recipent,
+            VotingDeadline = checked(debatingDuration + Block.Number),
+            Creator = Message.Sender,
+            Open = true
+        };
+
+        SetProposal(LastProposalIndex, proposal);
+
+        Log(new ProposalAddedLog
+        {
+            ProposalId = LastProposalIndex,
+            Recipent = recipent,
+            Amount = amount,
+            Description = description
+        });
+
+        return LastProposalIndex++;
+    }
+
+    public void Vote(uint proposalId, bool vote)
+    {
+        Assert(IsWhitelisted(Message.Sender));
+
+        var proposal = GetProposal(proposalId);
+
+        Assert(proposal.VotingDeadline > Block.Number, "Voting is closed.");
+
+        SetVote(proposalId, Message.Sender, ToVote(vote));
+
+    }
+
+    private void SetVote(uint proposalId, Address address, Votes vote)
+    {
+        var currentVote = (Votes)GetVote(proposalId, address);
+
+        if (currentVote == vote)
+        {
+            return;
         }
 
-        public DAOContract(ISmartContractState state)
-            : base(state)
+        Unvote(proposalId, currentVote);
+
+        State.SetUInt32($"Vote:{proposalId}:{address}", (uint)vote);
+
+        if (vote == Votes.Yes)
         {
-
+            SetYesVotes(proposalId, GetYesVotes(proposalId) + 1);
         }
+        else
+        {
+            SetNoVotes(proposalId, GetNoVotes(proposalId) + 1);
+        }
+    }
 
+    private void Unvote(uint proposalId, Votes currentVote)
+    {
+        switch (currentVote)
+        {
+            case Votes.None: break;
+            case Votes.Yes: SetYesVotes(proposalId, GetYesVotes(proposalId) - 1); break;
+            case Votes.No: SetNoVotes(proposalId, GetNoVotes(proposalId) - 1); break;
+        }
+    }
 
+    private Votes ToVote(bool vote) => vote ? Votes.Yes : Votes.No;
+
+    public void ExecuteProposal(uint proposalId)
+    {
+        Assert(IsWhitelisted(Message.Sender));
+
+        var proposal = GetProposal(proposalId);
+        var yesVotes = GetYesVotes(proposalId);
+        var noVotes = GetNoVotes(proposalId);
+
+        Assert(yesVotes > noVotes, "The proposal voting is not passed.");
+        Assert(yesVotes >= MinQuorum, "Min quorum for proposal is not reached.");
+
+        Assert(proposal.Open, "The proposal is closed.");
+        Assert(proposal.VotingDeadline < Block.Number, "Voting is still open for the proposal.");
+        Assert(proposal.Amount < Balance, "Insufficient balance.");
+
+        proposal.Open = false;
+
+        SetProposal(proposalId, proposal);
+        var result = Transfer(proposal.Recipient, proposal.Amount);
+
+        Assert(result.Success, "Transfer failed.");
+
+    }
+
+    public void Deposit()
+    {
+
+    }
+    public enum Votes : uint
+    {
+        None,
+        No,
+        Yes
+    }
+
+    struct ProposalAddedLog
+    {
+        public Address Recipent;
+        public uint ProposalId;
+        public ulong Amount;
+        public string Description;
     }
 }
---
+++ Testnet/DAOContract/DAOContract/PrivateYesNoVote.cs
@@ -0,0 +1,117 @@
+﻿using Stratis.SmartContracts;
+
+public class PrivateYesNoVote : SmartContract
+{
+    private const char Yes = 'y';
+    private const char No = 'n';
+
+    public PrivateYesNoVote(ISmartContractState smartContractState, ulong duration, byte[] addresses)
+        : base(smartContractState)
+    {
+        VotePeriodEndBlock = checked(Block.Number + duration);
+        Owner = Message.Sender;
+        SetVotersExecute(addresses);
+    }
+    
+    public Address Owner
+    {
+        get => State.GetAddress(nameof(Owner));
+        private set => State.SetAddress(nameof(Owner), value);
+    }
+
+    public ulong VotePeriodEndBlock
+    {
+        get => State.GetUInt64(nameof(VotePeriodEndBlock));
+        private set => State.SetUInt64(nameof(VotePeriodEndBlock), value);
+    }
+
+    public uint YesVotes
+    {
+        get => State.GetUInt32(nameof(YesVotes));
+        private set => State.SetUInt32(nameof(YesVotes), value);
+    }
+
+    public uint NoVotes
+    {
+        get => State.GetUInt32(nameof(NoVotes));
+        private set => State.SetUInt32(nameof(NoVotes), value);
+    }
+
+    private void SetVoterExecute(Address address)
+    {
+        State.SetBool($"Voter:{address}", true);
+    }
+
+    public bool IsVoter(Address address)
+    {
+        return State.GetBool($"Voter:{address}");
+    }
+
+    public char GetVote(Address address)
+    {
+        return State.GetChar($"Vote:{address}");
+    }
+
+    private void SetVote(Address address, char vote)
+    {
+        State.SetChar($"Vote:{address}", vote);
+    }
+
+    public void SetVoter(Address address)
+    {
+        EnsureOwnerOnly();
+        SetVoterExecute(address);
+    }
+
+    public void SetVoters(byte[] addresses)
+    {
+        EnsureOwnerOnly();
+        SetVotersExecute(addresses);
+    }
+
+    private void SetVotersExecute(byte[] addresses)
+    {
+        var addressList = Serializer.ToArray<Address>(addresses);
+
+        foreach (var address in addressList)
+        {
+            SetVoterExecute(address);
+        }
+    }
+
+    public void Vote(bool vote)
+    {
+        Assert(IsVoter(Message.Sender), "Sender is not authorized to vote.");
+        Assert(GetVote(Message.Sender) == default(char), "Sender has already voted.");
+        Assert(Block.Number <= VotePeriodEndBlock, "Voting period has ended.");
+
+        char voteType;
+
+        if (vote)
+        {
+            voteType = Yes;
+            YesVotes++;
+        }
+        else
+        {
+            voteType = No;
+            NoVotes++;
+        }
+
+        SetVote(Message.Sender, voteType);
+
+        Log(new VoteEvent { Voter = Message.Sender, Vote = vote });
+    }
+
+    private void EnsureOwnerOnly()
+    {
+        Assert(Message.Sender == Owner, "Must be the contract owner to authorize voters.");
+    }
+
+    public struct VoteEvent
+    {
+        [Index]
+        public Address Voter;
+        public bool Vote;
+    }
+}
