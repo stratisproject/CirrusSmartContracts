66f931c506fa3c03da7e1bc8aa4ce81e4e063fc7
YakupIpek <yakupipek@gmail.com>
2021-03-01 04:58:50 +0300

    Initial
    (cherry picked from commit 03d7efcce0bb20581711eeaa27c71c7d559b63ca)

---
+++ Testnet/DAOContract/DAOContract.Tests/DAOContractTests.cs
@@ -0,0 +1,32 @@
+﻿using DAOContract;
+using DividendTokenContract.Tests;
+using Moq;
+using Stratis.SmartContracts;
+using Xunit;
+
+namespace DAOContract.Tests
+{
+    public class DividendTokenTests
+    {
+        private readonly IPersistentState state;
+
+        private readonly Mock<ISmartContractState> mContractState;
+        private readonly Mock<IContractLogger> mContractLogger;
+        private readonly Mock<IInternalTransactionExecutor> mTransactionExecutor;
+
+        private readonly Address owner;
+
+
+        public DividendTokenTests()
+        {
+            state = new InMemoryState();
+            mContractState = new Mock<ISmartContractState>();
+            mContractLogger = new Mock<IContractLogger>();
+            mTransactionExecutor = new Mock<IInternalTransactionExecutor>();
+            mContractState.Setup(s => s.PersistentState).Returns(state);
+            mContractState.Setup(s => s.ContractLogger).Returns(mContractLogger.Object);
+            mContractState.Setup(s => s.InternalTransactionExecutor).Returns(mTransactionExecutor.Object);
+            owner = "0x0000000000000000000000000000000000000001".HexToAddress();
+        }
+    }
+}
---
+++ Testnet/DAOContract/DAOContract/DAOContract.cs
@@ -0,0 +1,22 @@
+﻿using Stratis.SmartContracts;
+
+namespace DAOContract
+{
+    [Deploy]
+    public class DAOContract : SmartContract
+    {
+        public ulong Dividends
+        {
+            get => State.GetUInt64(nameof(Dividends));
+            private set => State.SetUInt64(nameof(Dividends), value);
+        }
+
+        public DAOContract(ISmartContractState state)
+            : base(state)
+        {
+
+        }
+
+
+    }
+}
