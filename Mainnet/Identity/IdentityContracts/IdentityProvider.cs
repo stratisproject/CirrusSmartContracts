3cf013787ca0587c2cdb9a8f2a99602e2cfbec6d
YakupIpek <yakupipek@gmail.com>
2021-04-24 23:56:25 +0300

    Style fixes

--- Mainnet/Identity/IdentityContracts/IdentityProvider.cs
+++ Mainnet/Identity/IdentityContracts/IdentityProvider.cs
@@ -2,14 +2,14 @@
 
 /// <summary>
 /// A constrained version of ERC780.
-/// This contract only holds claims from one identity provider, the "Owner".
+/// contract only holds claims from one identity provider, the "Owner".
 /// </summary>
 public class IdentityProvider : SmartContract
 {
     private Address Owner
     {
-        get => this.PersistentState.GetAddress(nameof(Owner));
-        set => this.PersistentState.SetAddress(nameof(Owner), value);
+        get => PersistentState.GetAddress(nameof(Owner));
+        set => PersistentState.SetAddress(nameof(Owner), value);
     }
 
     public IdentityProvider(ISmartContractState state) : base(state)
@@ -19,17 +19,17 @@ public class IdentityProvider : SmartContract
 
     public void ChangeOwner(Address newOwner)
     {
-        Assert(this.Owner == Message.Sender);
-        this.Owner = newOwner;
+        Assert(Owner == Message.Sender);
+        Owner = newOwner;
     }
 
     public void AddClaim(Address issuedTo, uint topic, byte[] data)
     {
-        Assert(this.Owner == Message.Sender);
+        Assert(Owner == Message.Sender);
 
-        this.SetClaim(issuedTo, topic, data);
+        SetClaim(issuedTo, topic, data);
 
-        this.Log(new ClaimChanged
+        Log(new ClaimChanged
         {
             IssuedTo = issuedTo,
             Topic = topic,
@@ -39,10 +39,10 @@ public class IdentityProvider : SmartContract
 
     public void RemoveClaim(Address issuedTo, uint topic)
     {
-        Assert(this.Owner == Message.Sender);
+        Assert(Owner == Message.Sender);
 
         // Nothing to delete.
-        byte[] oldData = this.GetClaim(issuedTo, topic);
+        byte[] oldData = GetClaim(issuedTo, topic);
 
         if (oldData.Length == 0)
         {
@@ -51,7 +51,7 @@ public class IdentityProvider : SmartContract
 
         ClearClaim(issuedTo, topic);
 
-        this.Log(new ClaimRemoved
+        Log(new ClaimRemoved
         {
             IssuedTo = issuedTo,
             Topic = topic,
@@ -63,17 +63,17 @@ public class IdentityProvider : SmartContract
 
     public byte[] GetClaim(Address issuedTo, uint topic)
     {
-        return this.PersistentState.GetBytes($"Claim[{issuedTo}][{topic}]");
+        return PersistentState.GetBytes($"Claim[{issuedTo}][{topic}]");
     }
 
     private void SetClaim(Address issuedTo, uint topic, byte[] data)
     {
-        this.PersistentState.SetBytes($"Claim[{issuedTo}][{topic}]", data);
+        PersistentState.SetBytes($"Claim[{issuedTo}][{topic}]", data);
     }
 
     private void ClearClaim(Address issuedTo, uint topic)
     {
-        this.PersistentState.Clear($"Claim[{issuedTo}][{topic}]");
+        PersistentState.Clear($"Claim[{issuedTo}][{topic}]");
     }
     #region Events
 
