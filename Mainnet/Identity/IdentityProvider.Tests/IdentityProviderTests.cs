542b8cbbd1f5e310b4a15642736c2b845ce44d20
YakupIpek <yakupipek@gmail.com>
2021-04-24 23:54:13 +0300

    Fixed tests

--- Mainnet/Identity/IdentityProvider.Tests/IdentityProviderTests.cs
+++ Mainnet/Identity/IdentityProvider.Tests/IdentityProviderTests.cs
@@ -7,6 +7,8 @@ namespace IdentityContracts.Tests
 {
     public class IdentityProviderTests
     {
+        private const uint Topic = 1;
+        private static readonly byte[] ClaimData = new byte[] { 0, 1, 3, 4 };
         private readonly Mock<ISmartContractState> mockContractState;
         private readonly Mock<IPersistentState> mockPersistentState;
         private readonly Mock<IContractLogger> mockContractLogger;
@@ -16,9 +18,6 @@ namespace IdentityContracts.Tests
         private readonly Address claimReceiver;
         private readonly Address attacker;
         private readonly Address owner2;
-        private readonly Address contractAddress;
-        private const uint Topic1 = 1;
-        private static readonly byte[] ClaimData = new byte[]{ 0, 1, 3, 4 };
 
         public IdentityProviderTests()
         {
@@ -35,7 +34,6 @@ namespace IdentityContracts.Tests
             this.claimReceiver = "0x0000000000000000000000000000000000000002".HexToAddress();
             this.attacker = "0x0000000000000000000000000000000000000003".HexToAddress();
             this.owner2 = "0x0000000000000000000000000000000000000004".HexToAddress();
-            this.contractAddress = "0x0000000000000000000000000000000000000005".HexToAddress();
         }
 
         [Fact]
@@ -56,8 +54,8 @@ namespace IdentityContracts.Tests
 
             this.mockMessage.Setup(x => x.Sender).Returns(this.attacker);
 
-            Assert.Throws<SmartContractAssertException>(() => contract.AddClaim(this.claimReceiver, Topic1, ClaimData));
-            Assert.Throws<SmartContractAssertException>(() => contract.RemoveClaim(this.claimReceiver, Topic1));
+            Assert.Throws<SmartContractAssertException>(() => contract.AddClaim(this.claimReceiver, Topic, ClaimData));
+            Assert.Throws<SmartContractAssertException>(() => contract.RemoveClaim(this.claimReceiver, Topic));
             Assert.Throws<SmartContractAssertException>(() => contract.ChangeOwner(this.attacker));
         }
 
@@ -78,16 +76,16 @@ namespace IdentityContracts.Tests
         {
             var contract = new IdentityProvider(this.mockContractState.Object);
             this.mockPersistentState.Setup(x => x.GetAddress("Owner")).Returns(this.owner);
-            this.mockPersistentState.Setup(x => x.GetBytes(It.IsAny<string>())).Returns((byte[]) null);
+            this.mockPersistentState.Setup(x => x.GetBytes(It.IsAny<string>())).Returns((byte[])null);
             this.mockMessage.Setup(x => x.Sender).Returns(this.owner);
 
-            contract.AddClaim(this.claimReceiver, Topic1, ClaimData);
+            contract.AddClaim(this.claimReceiver, Topic, ClaimData);
 
-            this.mockPersistentState.Verify(x=>x.SetBytes($"Claim[{this.claimReceiver}][{Topic1}]", ClaimData));
-            this.mockContractLogger.Verify(x=>x.Log(It.IsAny<ISmartContractState>(), It.Is<IdentityProvider.ClaimAdded>(y =>
-                y.Topic == Topic1 
-                && y.Data == ClaimData
-                && y.IssuedTo == this.claimReceiver)));
+            this.mockPersistentState.Verify(x => x.SetBytes($"Claim[{this.claimReceiver}][{Topic}]", ClaimData));
+            this.mockContractLogger.Verify(x => x.Log(It.IsAny<ISmartContractState>(), It.Is<IdentityProvider.ClaimChanged>(y =>
+                  y.Topic == Topic
+                  && y.Data == ClaimData
+                  && y.IssuedTo == this.claimReceiver)));
         }
 
     }
