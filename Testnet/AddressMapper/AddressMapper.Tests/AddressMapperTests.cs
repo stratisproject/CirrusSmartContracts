368ecc8d0ebf742b3f6d0bf36696fb579affe308
YakupIpek <yakupipek@gmail.com>
2021-04-26 22:57:42 +0300

    Testnet updated

--- Testnet/AddressMapper/AddressMapper.Tests/AddressMapperTests.cs
+++ Testnet/AddressMapper/AddressMapper.Tests/AddressMapperTests.cs
@@ -1,10 +1,10 @@
+using AddressMapperTests;
 using Moq;
-using Stratis.SmartContracts.CLR;
 using Stratis.SmartContracts;
 using Xunit;
 using static AddressMapper;
 
-namespace Tests
+namespace AddressMapperTests
 {
     public class AddressMapperTests
     {
@@ -12,7 +12,7 @@ namespace Tests
         private readonly Mock<IContractLogger> mContractLogger;
         private readonly Mock<IInternalTransactionExecutor> mInternalExecutor;
 
-        private readonly IPersistentState persistentState;
+        private readonly IPersistentState state;
 
         private readonly Address contractAddress;
         private readonly Address primaryAddress;
@@ -22,10 +22,10 @@ namespace Tests
         public AddressMapperTests()
         {
             mContractLogger = new Mock<IContractLogger>();
-            persistentState = new InMemoryState();
+            state = new InMemoryState();
             mContractState = new Mock<ISmartContractState>();
             mInternalExecutor = new Mock<IInternalTransactionExecutor>();
-            mContractState.Setup(x => x.PersistentState).Returns(persistentState);
+            mContractState.Setup(x => x.PersistentState).Returns(state);
             mContractState.Setup(x => x.ContractLogger).Returns(mContractLogger.Object);
             mContractState.Setup(x => x.InternalTransactionExecutor).Returns(mInternalExecutor.Object);
             contractAddress = "0x0000000000000000000000000000000000000001".HexToAddress();
@@ -39,7 +39,7 @@ namespace Tests
         [InlineData(Status.Pending)]
         public void MappAddress_Fails_If_SecondaryAddress_Is_Mapped_Already(Status status)
         {
-            persistentState.SetStruct($"MappingInfo:{secondaryAddress}", new MappingInfo { Primary = primaryAddress, Status = (int)status });
+            state.SetStruct($"MappingInfo:{secondaryAddress}", new MappingInfo { Primary = primaryAddress, Status = (int)status });
 
             mContractState.Setup(x => x.Message).Returns(new Message(contractAddress, primaryAddress, 0));
 
@@ -51,15 +51,13 @@ namespace Tests
         [Fact]
         public void MappAddress_Maps_PrimaryAddres()
         {
-            persistentState.SetStruct($"MappingInfo:{secondaryAddress}", new MappingInfo { Primary = primaryAddress, Status = (int)Status.NoStatus });
-
             mContractState.Setup(x => x.Message).Returns(new Message(contractAddress, primaryAddress, 0));
 
             var contract = new AddressMapper(mContractState.Object, ownerAddress);
 
             contract.MapAddress(secondaryAddress);
 
-            Assert.Equal(new MappingInfo { Primary = primaryAddress, Status = (int)Status.Pending }, persistentState.GetStruct<MappingInfo>($"MappingInfo:{secondaryAddress}"));
+            Assert.Equal(new MappingInfo { Primary = primaryAddress, Status = (int)Status.Pending }, state.GetStruct<MappingInfo>($"MappingInfo:{secondaryAddress}"));
         }
 
         [Fact]
@@ -77,7 +75,7 @@ namespace Tests
         [InlineData(Status.NoStatus)]
         public void Approve_Fails_If_State_Is_Not_InPending_State(Status status)
         {
-            persistentState.SetStruct($"MappingInfo:{secondaryAddress}", new MappingInfo { Primary = primaryAddress, Status = (int)status });
+            state.SetStruct($"MappingInfo:{secondaryAddress}", new MappingInfo { Primary = primaryAddress, Status = (int)status });
 
             mContractState.Setup(x => x.Message).Returns(new Message(contractAddress, ownerAddress, 0));
 
@@ -89,7 +87,7 @@ namespace Tests
         [Fact]
         public void Approve_Approves_Mapping()
         {
-            persistentState.SetStruct($"MappingInfo:{secondaryAddress}", new MappingInfo { Primary = primaryAddress, Status = (int)Status.Pending });
+            state.SetStruct($"MappingInfo:{secondaryAddress}", new MappingInfo { Primary = primaryAddress, Status = (int)Status.Pending });
 
             mContractState.Setup(x => x.Message).Returns(new Message(contractAddress, ownerAddress, 0));
 
@@ -97,9 +95,9 @@ namespace Tests
 
             contract.Approve(secondaryAddress);
 
-            Assert.Equal(secondaryAddress, persistentState.GetAddress($"SecondaryAddress:{primaryAddress}"));
+            Assert.Equal(secondaryAddress, state.GetAddress($"SecondaryAddress:{primaryAddress}"));
 
-            Assert.Equal(new MappingInfo { Primary = primaryAddress, Status = (int)Status.Approved }, persistentState.GetStruct<MappingInfo>($"MappingInfo:{secondaryAddress}"));
+            Assert.Equal(new MappingInfo { Primary = primaryAddress, Status = (int)Status.Approved }, state.GetStruct<MappingInfo>($"MappingInfo:{secondaryAddress}"));
 
             mContractLogger.Verify(m => m.Log(mContractState.Object, new AddressMappedLog { Primary = primaryAddress, Secondary = secondaryAddress }));
         }
@@ -119,7 +117,7 @@ namespace Tests
         [InlineData(Status.NoStatus)]
         public void Reject_Fails_If_State_Is_Not_InPending_State(Status status)
         {
-            persistentState.SetStruct($"MappingInfo:{secondaryAddress}", new MappingInfo { Primary = primaryAddress, Status = (int)status });
+            state.SetStruct($"MappingInfo:{secondaryAddress}", new MappingInfo { Primary = primaryAddress, Status = (int)status });
 
             mContractState.Setup(x => x.Message).Returns(new Message(contractAddress, ownerAddress, 0));
 
@@ -131,7 +129,7 @@ namespace Tests
         [Fact]
         public void Reject_Rejects_Mapping()
         {
-            persistentState.SetStruct($"MappingInfo:{secondaryAddress}", new MappingInfo { Primary = primaryAddress, Status = (int)Status.Pending });
+            state.SetStruct($"MappingInfo:{secondaryAddress}", new MappingInfo { Primary = primaryAddress, Status = (int)Status.Pending });
 
             mContractState.Setup(x => x.Message).Returns(new Message(contractAddress, ownerAddress, 0));
 
@@ -139,7 +137,33 @@ namespace Tests
 
             contract.Reject(secondaryAddress);
 
-            Assert.Equal(default(MappingInfo), persistentState.GetStruct<MappingInfo>($"MappingInfo:{secondaryAddress}"));
+            Assert.Equal(default(MappingInfo), state.GetStruct<MappingInfo>($"MappingInfo:{secondaryAddress}"));
+        }
+
+        [Fact]
+        public void GetPrimaryAddress_Get_None_Approved_PrimaryAddres_Fails()
+        {
+            state.SetStruct($"MappingInfo:{secondaryAddress}", new MappingInfo { Primary = primaryAddress, Status = (int)Status.Pending });
+
+            mContractState.Setup(x => x.Message).Returns(new Message(contractAddress, ownerAddress, 0));
+
+            var contract = new AddressMapper(mContractState.Object, ownerAddress);
+
+            Assert.Throws<SmartContractAssertException>(()=> contract.GetPrimaryAddress(secondaryAddress));
+        }
+
+        [Fact]
+        public void GetPrimaryAddress_Get_Approved_PrimaryAddres_Success()
+        {
+            state.SetStruct($"MappingInfo:{secondaryAddress}", new MappingInfo { Primary = primaryAddress, Status = (int)Status.Approved });
+
+            mContractState.Setup(x => x.Message).Returns(new Message(contractAddress, ownerAddress, 0));
+
+            var contract = new AddressMapper(mContractState.Object, ownerAddress);
+
+            var result = contract.GetPrimaryAddress(secondaryAddress);
+
+            Assert.Equal(primaryAddress, result);
         }
     }
 }
--- Testnet/AddressMapper/AddressMapper/AddressMapper.cs
+++ Testnet/AddressMapper/AddressMapper/AddressMapper.cs
@@ -3,55 +3,71 @@ using Stratis.SmartContracts;
 
 public class AddressMapper : SmartContract
 {
-    public Address GetSecondaryAddress(Address primary) => PersistentState.GetAddress($"SecondaryAddress:{primary}");
-    private void SetSecondaryAddress(Address primary, Address secondary) => PersistentState.SetAddress($"SecondaryAddress:{primary}", secondary);
+    public Address GetSecondaryAddress(Address primary) => State.GetAddress($"SecondaryAddress:{primary}");
+    private void SetSecondaryAddress(Address primary, Address secondary) => State.SetAddress($"SecondaryAddress:{primary}", secondary);
 
-    private MappingInfo GetMappingInfo(Address secondary) => PersistentState.GetStruct<MappingInfo>($"MappingInfo:{secondary}");
-    private void SetMappingInfo(Address secondary, MappingInfo value) => PersistentState.SetStruct($"MappingInfo:{secondary}", value);
-    
-    private void ClearMappingInfo(Address secondary) => PersistentState.Clear($"MappingInfo:{secondary}");
+    public MappingInfo GetMapping(Address secondary) => State.GetStruct<MappingInfo>($"Mapping:{secondary}");
+    private void SetMapping(Address secondary, MappingInfo value) => State.SetStruct($"Mapping:{secondary}", value);
+
+    private void ClearMappingInfo(Address secondary) => State.Clear($"MappingInfo:{secondary}");
 
     public Address Owner
     {
-        get => PersistentState.GetAddress(nameof(Owner));
-        private set => PersistentState.SetAddress(nameof(Owner), value);
+        get => State.GetAddress(nameof(Owner));
+        private set => State.SetAddress(nameof(Owner), value);
     }
 
-    public AddressMapper(ISmartContractState smartContractState, Address Owner) : base(smartContractState)
+    public AddressMapper(ISmartContractState smartContractState, Address owner) : base(smartContractState)
     {
-        this.Owner = Owner;
+        this.Owner = owner;
     }
 
     public void MapAddress(Address secondary)
     {
-        Assert(GetMappingInfo(secondary).Status == (int)Status.NoStatus);
+        Assert(GetMapping(secondary).Status == (int)Status.NoStatus);
 
-        SetMappingInfo(secondary, new MappingInfo { Primary = Message.Sender, Status = (int)Status.Pending });
+        SetMapping(secondary, new MappingInfo { Primary = Message.Sender, Status = (int)Status.Pending });
     }
 
     public void Approve(Address secondary)
     {
         EnsureAdminOnly();
-        var mapping = GetMappingInfo(secondary);
+        var mapping = GetMapping(secondary);
         Assert(mapping.Status == (int)Status.Pending, "Mapping is not in pending state.");
 
         SetSecondaryAddress(mapping.Primary, secondary);
-        SetMappingInfo(secondary, new MappingInfo { Primary = mapping.Primary, Status = (int)Status.Approved });
-        
+        SetMapping(secondary, new MappingInfo { Primary = mapping.Primary, Status = (int)Status.Approved });
+
         Log(new AddressMappedLog { Primary = mapping.Primary, Secondary = secondary });
     }
 
     public void Reject(Address secondary)
     {
         EnsureAdminOnly();
-        var mapping = GetMappingInfo(secondary);
+        var mapping = GetMapping(secondary);
         Assert(mapping.Status == (int)Status.Pending, "Mapping is not in pending state.");
 
         ClearMappingInfo(secondary); // same address can be mapped again. 
     }
 
-    public string GetStatus(Address secondary) => GetMappingInfo(secondary).Status.ToString();
-    public Address GetPrimaryAddress(Address secondary) => GetMappingInfo(secondary).Primary;
+    public Address GetPrimaryAddress(Address secondary)
+    {
+        var mapping = GetMapping(secondary);
+
+        Assert(mapping.Status == (int)Status.Approved, "The mapping is not approved.");
+
+        return mapping.Primary;
+    }
+
+    public void ChangeOwner(Address owner)
+    {
+        EnsureAdminOnly();
+
+        Assert(owner != Address.Zero, $"The {nameof(owner)} parameter can not be default(zero) address.");
+
+        this.Owner = owner;
+    }
+
     public void EnsureAdminOnly() => Assert(this.Owner == Message.Sender, "Only contract owner can access.");
 
     public enum Status
@@ -63,6 +79,7 @@ public class AddressMapper : SmartContract
 
     public struct MappingInfo
     {
+        [Index]
         public Address Primary;
         public int Status;
     }
