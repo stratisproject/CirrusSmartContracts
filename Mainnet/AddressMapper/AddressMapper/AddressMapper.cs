28d0407927410929c5aee3270b700dd5d2be6cee
YakupIpek <yakupipek@gmail.com>
2021-04-26 22:48:36 +0300

    Renaming

--- Mainnet/AddressMapper/AddressMapper/AddressMapper.cs
+++ Mainnet/AddressMapper/AddressMapper/AddressMapper.cs
@@ -6,8 +6,8 @@ public class AddressMapper : SmartContract
     public Address GetSecondaryAddress(Address primary) => State.GetAddress($"SecondaryAddress:{primary}");
     private void SetSecondaryAddress(Address primary, Address secondary) => State.SetAddress($"SecondaryAddress:{primary}", secondary);
 
-    private MappingInfo GetMappingInfo(Address secondary) => State.GetStruct<MappingInfo>($"MappingInfo:{secondary}");
-    private void SetMappingInfo(Address secondary, MappingInfo value) => State.SetStruct($"MappingInfo:{secondary}", value);
+    public MappingInfo GetMapping(Address secondary) => State.GetStruct<MappingInfo>($"Mapping:{secondary}");
+    private void SetMapping(Address secondary, MappingInfo value) => State.SetStruct($"Mapping:{secondary}", value);
 
     private void ClearMappingInfo(Address secondary) => State.Clear($"MappingInfo:{secondary}");
 
@@ -24,19 +24,19 @@ public class AddressMapper : SmartContract
 
     public void MapAddress(Address secondary)
     {
-        Assert(GetMappingInfo(secondary).Status == (int)Status.NoStatus);
+        Assert(GetMapping(secondary).Status == (int)Status.NoStatus);
 
-        SetMappingInfo(secondary, new MappingInfo { Primary = Message.Sender, Status = (int)Status.Pending });
+        SetMapping(secondary, new MappingInfo { Primary = Message.Sender, Status = (int)Status.Pending });
     }
 
     public void Approve(Address secondary)
     {
         EnsureAdminOnly();
-        var mapping = GetMappingInfo(secondary);
+        var mapping = GetMapping(secondary);
         Assert(mapping.Status == (int)Status.Pending, "Mapping is not in pending state.");
 
         SetSecondaryAddress(mapping.Primary, secondary);
-        SetMappingInfo(secondary, new MappingInfo { Primary = mapping.Primary, Status = (int)Status.Approved });
+        SetMapping(secondary, new MappingInfo { Primary = mapping.Primary, Status = (int)Status.Approved });
 
         Log(new AddressMappedLog { Primary = mapping.Primary, Secondary = secondary });
     }
@@ -44,20 +44,15 @@ public class AddressMapper : SmartContract
     public void Reject(Address secondary)
     {
         EnsureAdminOnly();
-        var mapping = GetMappingInfo(secondary);
+        var mapping = GetMapping(secondary);
         Assert(mapping.Status == (int)Status.Pending, "Mapping is not in pending state.");
 
         ClearMappingInfo(secondary); // same address can be mapped again. 
     }
 
-    public MappingInfo GetMapping(Address secondary)
-    {
-        return GetMappingInfo(secondary);
-    }
-
     public Address GetPrimaryAddress(Address secondary)
     {
-        var mapping = GetMappingInfo(secondary);
+        var mapping = GetMapping(secondary);
 
         Assert(mapping.Status == (int)Status.Approved, "The mapping is not approved.");
 
