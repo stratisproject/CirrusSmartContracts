f640586531c11fe5d03194ad5371831c1263b239
YakupIpek <yakupipek@gmail.com>
2021-05-31 21:08:24 +0300

    Auction logic added

--- Testnet/NFTExchange/NFTExchange/NFTExchange.cs
+++ Testnet/NFTExchange/NFTExchange/NFTExchange.cs
@@ -21,11 +21,17 @@ public class NFTExchange : SmartContract
     public NFTExchange(ISmartContractState state)
         : base(state)
     {
+        EnsureNotPayable();
         Owner = Message.Sender;
     }
 
+    /// <summary>
+    /// Ensure the token is approved on the non fungible contract for exchange contract as pre-condition.
+    /// </summary>
     public void Sale(Address contract, ulong tokenId, ulong price)
     {
+        EnsureNotPayable();
+
         Assert(price > 0, "Price should be higher than zero.");
 
         var tokenOwner = GetOwner(contract, tokenId);
@@ -60,6 +66,7 @@ public class NFTExchange : SmartContract
 
     public void CancelSale(Address contract, ulong tokenId)
     {
+        EnsureNotPayable();
         var saleInfo = GetSaleInfo(contract, tokenId);
 
         Assert(saleInfo.Seller != Address.Zero, "The token is not on sale");
@@ -112,6 +119,7 @@ public class NFTExchange : SmartContract
 
     public void BlacklistToken(Address address)
     {
+        EnsureNotPayable();
         EnsureOwnerOnly();
 
         if (State.IsContract(address))
@@ -134,6 +142,7 @@ public class NFTExchange : SmartContract
 
     public void WhitelistToken(Address address)
     {
+        EnsureNotPayable();
         EnsureOwnerOnly();
 
         if (State.IsContract(address))
@@ -144,6 +153,7 @@ public class NFTExchange : SmartContract
 
     public void WhitelistTokens(byte[] addresses)
     {
+        EnsureNotPayable();
         EnsureOwnerOnly();
         foreach (var address in Serializer.ToArray<Address>(addresses))
         {
@@ -160,6 +170,137 @@ public class NFTExchange : SmartContract
     }
 
     private void EnsureOwnerOnly() => Assert(this.Owner == Message.Sender, "The method is owner only.");
+    private void EnsureNotPayable() => Assert(Message.Value == 0, "The method is not payable.");
+
+    #region Auction Logic
+
+    private void SetAuctionInfo(Address contract, ulong tokenId, AuctionInfo auctionInfo)
+    {
+        State.SetStruct($"AuctionInfo:{contract}:{tokenId}", auctionInfo);
+    }
+
+    public AuctionInfo GetAuctionInfo(Address contract, ulong tokenId)
+    {
+        EnsureNotPayable();
+        return State.GetStruct<AuctionInfo>($"AuctionInfo:{contract}:{tokenId}");
+    }
+
+    public ulong GetBalance(Address address)
+    {
+        EnsureNotPayable();
+        return State.GetUInt64($"Balances[{address}]");
+    }
+
+    private void SetBalance(Address address, ulong balance)
+    {
+        State.SetUInt64($"Balances[{address}]", balance);
+    }
+
+    public void Auction(Address contract, ulong tokenId, ulong startingPrice, ulong duration)
+    {
+        EnsureNotPayable();
+
+        var tokenOwner = GetOwner(contract, tokenId);
+
+        Assert(tokenOwner == Address, "The token is already on sale.");
+
+        EnsureCallerCanOperate(contract, tokenOwner);
+
+        TransferToken(contract, tokenId, tokenOwner, Address);
+
+        var auction = new AuctionInfo
+        {
+            Seller = tokenOwner,
+            EndBlock = checked(Block.Number + duration),
+            StartingPrice = startingPrice,
+            Ended = false
+        };
+
+        SetAuctionInfo(contract, tokenId, auction);
+        Log(new AuctionCreatedLog { EndBlock = auction.EndBlock, Seller = auction.Seller, startingPrice = auction.StartingPrice });
+    }
+
+    public void Bid(Address contract, ulong tokenId)
+    {
+        var auction = GetAuctionInfo(contract, tokenId);
+
+        Assert(Block.Number < auction.EndBlock);
+        Assert(Message.Value > auction.HighestBid && Message.Value >= auction.StartingPrice);
+
+        if (auction.HighestBid > 0)
+        {
+            var balance = GetBalance(auction.HighestBidder);
+
+            SetBalance(auction.HighestBidder, balance + auction.HighestBid);
+        }
+
+        auction.HighestBidder = Message.Sender;
+        auction.HighestBid = Message.Value;
+
+        SetAuctionInfo(contract, tokenId, auction);
+
+        Log(new HighestBidUpdatedLog { Bidder = auction.HighestBidder, Bid = auction.HighestBid });
+    }
+
+    public bool Withdraw()
+    {
+        EnsureNotPayable();
+
+        var amount = GetBalance(Message.Sender);
+
+        Assert(amount > 0);
+
+        SetBalance(Message.Sender, 0);
+
+        var transferResult = Transfer(this.Message.Sender, amount);
+
+        if (!transferResult.Success)
+            SetBalance(this.Message.Sender, amount);
+
+        return transferResult.Success;
+    }
+
+    public void AuctionEnd(Address contract, ulong tokenId)
+    {
+        EnsureNotPayable();
+
+        var auction = GetAuctionInfo(contract, tokenId);
+
+        Assert(Block.Number >= auction.EndBlock);
+        
+        Assert(!auction.Ended);
+
+        auction.Ended = true;
+        
+        var result = Transfer(this.Owner, auction.HighestBid);
+        
+        Assert(result.Success, "Transfer failed.");
+
+        SafeTransferToken(contract, tokenId, Address, auction.HighestBidder);
+
+        Log(new AuctionEndedLog { Contract = contract, TokenId = tokenId, HighestBidder = auction.HighestBidder, HighestBid = auction.HighestBid });
+    }
+
+    public struct AuctionCreatedLog
+    {
+        public ulong EndBlock;
+        public ulong startingPrice;
+        public Address Seller;
+    }
+
+    public struct HighestBidUpdatedLog
+    {
+        public Address Bidder;
+        public ulong Bid;
+    }
+    public struct AuctionEndedLog
+    {
+        public Address Contract;
+        public ulong TokenId;
+        public Address HighestBidder;
+        public ulong HighestBid;
+    }
+    #endregion
 
     private struct TokenOnSaleLog
     {
@@ -189,5 +330,14 @@ public class NFTExchange : SmartContract
         public ulong Price;
         public Address Seller;
     }
-}
 
+    public struct AuctionInfo
+    {
+        public Address Seller;
+        public ulong HighestBid;
+        public Address HighestBidder;
+        public ulong EndBlock;
+        public bool Ended;
+        internal ulong StartingPrice;
+    }
+}
