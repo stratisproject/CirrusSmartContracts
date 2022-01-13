95d2c7fbe240ca8573496375e9d8bcfe8b36011a
YakupIpek <yakupipek@gmail.com>
2022-01-13 07:28:35 +0300

    Moved to mainnet folder

---
+++ Mainnet/NFTStore/NFTStore.Tests/TransferResult.cs
@@ -0,0 +1,19 @@
+﻿using Stratis.SmartContracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NFTStoreTests
+{
+    public class TransferResult : ITransferResult
+    {
+        public object ReturnValue { get; private set; }
+
+        public bool Success { get; private set; }
+
+        public static TransferResult Failed() => new TransferResult { Success = false };
+
+
+        public static TransferResult Succeed(object returnValue = null) => new TransferResult { Success = true, ReturnValue = returnValue };
+    }
+}
