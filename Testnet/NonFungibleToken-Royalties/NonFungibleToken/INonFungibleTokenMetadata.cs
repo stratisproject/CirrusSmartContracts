5ccecab3b95b1c7c7ebc11e8c970dabbd03326ec
phr4x <phr4x@users.noreply.github.com>
2022-02-17 00:00:00 +0000

    Add NFT royalty support and update sale and auction contracts

---
+++ Testnet/NonFungibleToken-Royalties/NonFungibleToken/INonFungibleTokenMetadata.cs
@@ -0,0 +1,24 @@
+﻿using Stratis.SmartContracts;
+
+namespace NonFungibleTokenContract
+{
+    public interface INonFungibleTokenMetadata
+    {
+        string Name { get; }
+        string Symbol { get; }
+
+        string TokenURI(UInt256 tokenId);
+    }
+
+    public interface IRoyaltyInfo
+    {
+        /// <summary>
+        /// Returns an object representing the royaltyInfo for the given contract.
+        /// The object[] is expected to include at least the receiver as the first param, and the royalty Amount as the second.
+        /// </summary>
+        /// <param name="tokenId"></param>
+        /// <param name="salePrice"></param>
+        /// <returns></returns>
+         object[] RoyaltyInfo(UInt256 tokenId, UInt256 salePrice);
+    }
+}
