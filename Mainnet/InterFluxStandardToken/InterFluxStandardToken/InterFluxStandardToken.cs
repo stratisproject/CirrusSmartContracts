eb6d002660898564a4ffb8642c7d94bd83fb9286
Iain McCain <iain.mccain@stratisplatform.com>
2022-03-02 11:28:07 +0000

    InterFluxStandardToken-Mainnet

---
+++ Mainnet/InterFluxStandardToken/InterFluxStandardToken/InterFluxStandardToken.cs
@@ -0,0 +1,297 @@
+﻿using Stratis.SmartContracts;
+using Stratis.SmartContracts.Standards;
+
+/// <summary>
+/// Implementation of a standard token contract for the Stratis Platform.
+/// </summary>
+[Deploy]
+public class InterFluxStandardToken : SmartContract, IStandardToken256, IOwnable, IMintable, IBurnable, IBurnableWithMetadata
+{
+    /// <summary>
+    /// Constructor used to create a new instance of the token. Assigns the total token supply to the creator of the contract.
+    /// </summary>
+    /// <param name="smartContractState">The execution state for the contract.</param>
+    /// <param name="totalSupply">The total token supply.</param>
+    /// <param name="name">The name of the token.</param>
+    /// <param name="symbol">The symbol used to identify the token.</param>
+    /// <param name="decimals">The amount of decimals for display and calculation purposes.</param>
+    /// <param name="nativeChain">The blockchain name of the token's native chain.</param>
+    /// <param name="nativeAddress">The contract address of the token's native blockchain if available.</param>
+    public InterFluxStandardToken(ISmartContractState smartContractState, UInt256 totalSupply, string name, string symbol,
+                                  byte decimals, string nativeChain, string nativeAddress) : base(smartContractState)
+    {
+        this.TotalSupply = totalSupply;
+        this.Name = name;
+        this.Symbol = symbol;
+        this.Decimals = decimals;
+        this.NativeChain = nativeChain;
+        this.NativeAddress = nativeAddress;
+        this.SetBalance(Message.Sender, totalSupply);
+        this.Owner = Message.Sender;
+    }
+
+    public string Symbol
+    {
+        get => State.GetString(nameof(this.Symbol));
+        private set => State.SetString(nameof(this.Symbol), value);
+    }
+
+    public string Name
+    {
+        get => State.GetString(nameof(this.Name));
+        private set => State.SetString(nameof(this.Name), value);
+    }
+
+    /// <inheritdoc />
+    public byte Decimals
+    {
+        get => State.GetBytes(nameof(this.Decimals))[0];
+        private set => State.SetBytes(nameof(this.Decimals), new[] { value });
+    }
+
+    /// <inheritdoc />
+    public UInt256 TotalSupply
+    {
+        get => State.GetUInt256(nameof(this.TotalSupply));
+        private set => State.SetUInt256(nameof(this.TotalSupply), value);
+    }
+
+    /// <inheritdoc />
+    public Address Owner
+    {
+        get => State.GetAddress(nameof(this.Owner));
+        private set => State.SetAddress(nameof(this.Owner), value);
+    }
+
+    public string NativeChain
+    {
+        get => State.GetString(nameof(this.NativeChain));
+        private set => State.SetString(nameof(this.NativeChain), value);
+    }
+
+    public string NativeAddress
+    {
+        get => State.GetString(nameof(this.NativeAddress));
+        private set => State.SetString(nameof(this.NativeAddress), value);
+    }
+
+    /// <inheritdoc />
+    public UInt256 GetBalance(Address address)
+    {
+        return State.GetUInt256($"Balance:{address}");
+    }
+
+    private void SetBalance(Address address, UInt256 value)
+    {
+        State.SetUInt256($"Balance:{address}", value);
+    }
+
+    /// <inheritdoc />
+    public bool TransferTo(Address to, UInt256 amount)
+    {
+        if (amount == 0)
+        {
+            Log(new TransferLog { From = Message.Sender, To = to, Amount = 0 });
+
+            return true;
+        }
+
+        UInt256 senderBalance = GetBalance(Message.Sender);
+
+        if (senderBalance < amount)
+        {
+            return false;
+        }
+
+        SetBalance(Message.Sender, senderBalance - amount);
+
+        SetBalance(to, checked(GetBalance(to) + amount));
+
+        Log(new TransferLog { From = Message.Sender, To = to, Amount = amount });
+
+        return true;
+    }
+
+    /// <inheritdoc />
+    public bool TransferFrom(Address from, Address to, UInt256 amount)
+    {
+        if (amount == 0)
+        {
+            Log(new TransferLog { From = from, To = to, Amount = 0 });
+
+            return true;
+        }
+
+        UInt256 senderAllowance = Allowance(from, Message.Sender);
+        UInt256 fromBalance = GetBalance(from);
+
+        if (senderAllowance < amount || fromBalance < amount)
+        {
+            return false;
+        }
+
+        SetApproval(from, Message.Sender, senderAllowance - amount);
+
+        SetBalance(from, fromBalance - amount);
+
+        SetBalance(to, checked(GetBalance(to) + amount));
+
+        Log(new TransferLog { From = from, To = to, Amount = amount });
+
+        return true;
+    }
+
+    /// <inheritdoc />
+    public bool Approve(Address spender, UInt256 currentAmount, UInt256 amount)
+    {
+        if (Allowance(Message.Sender, spender) != currentAmount)
+        {
+            return false;
+        }
+
+        SetApproval(Message.Sender, spender, amount);
+
+        Log(new ApprovalLog { Owner = Message.Sender, Spender = spender, Amount = amount, OldAmount = currentAmount });
+
+        return true;
+    }
+
+    private void SetApproval(Address owner, Address spender, UInt256 value)
+    {
+        State.SetUInt256($"Allowance:{owner}:{spender}", value);
+    }
+
+    /// <inheritdoc />
+    public UInt256 Allowance(Address owner, Address spender)
+    {
+        return State.GetUInt256($"Allowance:{owner}:{spender}");
+    }
+
+    /// <summary>
+    /// Secures method access by ensuring that only the owner of the contract is able to call a particular method.
+    /// </summary>
+    private void OnlyOwner()
+    {
+        Assert(Message.Sender == Owner, "Only the owner can call this method");
+    }
+
+    /// <inheritdoc />
+    public void RenounceOwnership()
+    {
+        TransferOwnership(Address.Zero);
+    }
+
+    /// <inheritdoc />
+    public void TransferOwnership(Address newOwner)
+    {
+        OnlyOwner();
+
+        Address previousOwner = this.Owner;
+
+        this.Owner = newOwner;
+
+        Log(new OwnershipTransferred { PreviousOwner = previousOwner, NewOwner = newOwner });
+    }
+
+    /// <inheritdoc />
+    public void Mint(Address account, UInt256 amount)
+    {
+        OnlyOwner();
+
+        UInt256 startingBalance = GetBalance(account);
+
+        SetBalance(account, startingBalance + amount);
+
+        Log(new TransferLog() { From = Address.Zero, To = account, Amount = amount });
+
+        this.TotalSupply += amount;
+
+        Log(new SupplyChangeLog()
+        {
+            PreviousSupply = (this.TotalSupply - amount),
+            TotalSupply = this.TotalSupply
+        });
+    }
+
+    /// <inheritdoc />
+    public bool Burn(UInt256 amount)
+    {
+        if (TransferTo(Address.Zero, amount))
+        {
+            this.TotalSupply -= amount;
+
+            Log(new SupplyChangeLog()
+            {
+                PreviousSupply = (this.TotalSupply - amount),
+                TotalSupply = this.TotalSupply
+            });
+
+            return true;
+        }
+
+        return false;
+    }
+
+    /// <inheritdoc />
+    public bool BurnWithMetadata(UInt256 amount, string metadata)
+    {
+        if (Burn(amount))
+        {
+            Log(new BurnMetadata() { From = Message.Sender, Amount = amount, Metadata = metadata });
+
+            return true;
+        }
+
+        return false;
+    }
+
+    public struct TransferLog
+    {
+        [Index]
+        public Address From;
+
+        [Index]
+        public Address To;
+
+        public UInt256 Amount;
+    }
+
+    public struct ApprovalLog
+    {
+        [Index]
+        public Address Owner;
+
+        [Index]
+        public Address Spender;
+
+        public UInt256 OldAmount;
+
+        public UInt256 Amount;
+    }
+
+    /// <summary>
+    /// Provides a record that ownership was transferred from one account to another.
+    /// </summary>
+    public struct OwnershipTransferred
+    {
+        [Index] public Address PreviousOwner;
+
+        [Index] public Address NewOwner;
+    }
+
+    public struct BurnMetadata
+    {
+        [Index] public Address From;
+
+        [Index] public string Metadata;
+
+        public UInt256 Amount;
+    }
+
+    public struct SupplyChangeLog
+    {
+        public UInt256 PreviousSupply;
+
+        public UInt256 TotalSupply;
+    }
+}
---
+++ Mainnet/InterFluxStandardToken/InterFluxStandardTokenTests/InterFluxStandardTokenTests.cs
@@ -0,0 +1,306 @@
+﻿using Moq;
+using Stratis.SmartContracts;
+using Stratis.SmartContracts.CLR;
+using Xunit;
+
+namespace InterFluxStandardTokenTests
+{
+    /// <summary>
+    /// These tests validate the functionality that differs between the original standard token and the extended version.
+    /// </summary>
+    public class InterFluxStandardTokenTests
+    {
+        private readonly Mock<ISmartContractState> mockContractState;
+        private readonly Mock<IPersistentState> mockPersistentState;
+        private readonly Mock<IContractLogger> mockContractLogger;
+        private Address owner;
+        private Address sender;
+        private Address contract;
+        private Address spender;
+        private Address destination;
+        private string name;
+        private string symbol;
+        private byte decimals;
+        private string nativeChain;
+        private string nativeAddress;
+
+        public InterFluxStandardTokenTests()
+        {
+            this.mockContractLogger = new Mock<IContractLogger>();
+            this.mockPersistentState = new Mock<IPersistentState>();
+            this.mockContractState = new Mock<ISmartContractState>();
+            this.mockContractState.Setup(s => s.PersistentState).Returns(this.mockPersistentState.Object);
+            this.mockContractState.Setup(s => s.ContractLogger).Returns(this.mockContractLogger.Object);
+            this.owner = "0x0000000000000000000000000000000000000001".HexToAddress();
+            this.sender = "0x0000000000000000000000000000000000000002".HexToAddress();
+            this.contract = "0x0000000000000000000000000000000000000003".HexToAddress();
+            this.spender = "0x0000000000000000000000000000000000000004".HexToAddress();
+            this.destination = "0x0000000000000000000000000000000000000005".HexToAddress();
+            this.name = "Test Token";
+            this.symbol = "TST";
+            this.decimals = 8;
+            this.nativeChain = "Ethereum";
+            this.nativeAddress = "0xa3c22370de5f9544f0c4de126b1e46ceadf0a51b";
+        }
+
+        [Fact]
+        public void Constructor_Assigns_Owner()
+        {
+            UInt256 totalSupply = 100_000;
+            this.mockContractState.Setup(m => m.Message).Returns(new Message(this.contract, this.owner, 0));
+
+            var standardToken = new InterFluxStandardToken(this.mockContractState.Object, totalSupply, this.name, this.symbol, this.decimals, this.nativeChain, this.nativeAddress);
+
+            // Verify that PersistentState was called with the contract owner
+            this.mockPersistentState.Verify(s => s.SetAddress($"Owner", this.owner));
+        }
+
+        [Theory]
+        [InlineData(" ")]
+        [InlineData("")]
+        [InlineData("Ethereum")]
+        public void Constructor_Assigns_NativeChain(string chain)
+        {
+            UInt256 totalSupply = 100_000;
+            this.mockContractState.Setup(m => m.Message).Returns(new Message(this.contract, this.owner, 0));
+
+            var standardToken = new InterFluxStandardToken(this.mockContractState.Object, totalSupply, this.name, this.symbol, this.decimals, chain, this.nativeAddress);
+
+            // Verify that PersistentState was called with the contract owner
+            this.mockPersistentState.Verify(s => s.SetString("NativeChain", chain));
+        }
+
+        [Theory]
+        [InlineData(" ")]
+        [InlineData("")]
+        [InlineData("0xa3c22370de5f9544f0c4de126b1e46ceadf0a51b")]
+        public void Constructor_Assigns_NativeAddress(string nativeAddress)
+        {
+            UInt256 totalSupply = 100_000;
+            this.mockContractState.Setup(m => m.Message).Returns(new Message(this.contract, this.owner, 0));
+
+            var standardToken = new InterFluxStandardToken(this.mockContractState.Object, totalSupply, this.name, this.symbol, this.decimals, this.nativeChain, nativeAddress);
+
+            // Verify that PersistentState was called with the contract owner
+            this.mockPersistentState.Verify(s => s.SetString("NativeAddress", nativeAddress));
+        }
+
+        [Fact]
+        public void RenounceOwnership_Succeeds_For_Owner()
+        {
+            UInt256 totalSupply = 100_000;
+            this.mockContractState.Setup(m => m.Message).Returns(new Message(this.contract, this.owner, 0));
+
+            var standardToken = new InterFluxStandardToken(this.mockContractState.Object, totalSupply, this.name, this.symbol, this.decimals, this.nativeChain, this.nativeAddress);
+
+            // Setup the owner of the contract
+            this.mockPersistentState.Setup(s => s.GetAddress($"Owner")).Returns(this.owner);
+
+            standardToken.RenounceOwnership();
+
+            // Verify that PersistentState was called to update the contract owner
+            this.mockPersistentState.Verify(s => s.SetAddress($"Owner", Address.Zero));
+
+            this.mockContractLogger.Verify(l => l.Log(It.IsAny<ISmartContractState>(), new InterFluxStandardToken.OwnershipTransferred() { PreviousOwner = this.owner, NewOwner = Address.Zero}));
+        }
+
+        [Fact]
+        public void RenounceOwnership_Fails_For_NonOwner()
+        {
+            UInt256 totalSupply = 100_000;
+            this.mockContractState.Setup(m => m.Message).Returns(new Message(this.contract, this.owner, 0));
+
+            var standardToken = new InterFluxStandardToken(this.mockContractState.Object, totalSupply, this.name, this.symbol, this.decimals, this.nativeChain, this.nativeAddress);
+
+            // Setup the owner of the contract
+            this.mockPersistentState.Setup(s => s.GetAddress($"Owner")).Returns(this.owner);
+
+            this.mockContractState.Setup(m => m.Message).Returns(new Message(this.contract, this.sender, 0));
+
+            Assert.ThrowsAny<SmartContractAssertException>(() => standardToken.RenounceOwnership());
+        }
+
+        [Fact]
+        public void TransferOwnership_Succeeds_For_Owner()
+        {
+            UInt256 totalSupply = 100_000;
+            this.mockContractState.Setup(m => m.Message).Returns(new Message(this.contract, this.owner, 0));
+
+            var standardToken = new InterFluxStandardToken(this.mockContractState.Object, totalSupply, this.name, this.symbol, this.decimals, this.nativeChain, this.nativeAddress);
+
+            // Setup the owner of the contract
+            this.mockPersistentState.Setup(s => s.GetAddress($"Owner")).Returns(this.owner);
+
+            standardToken.TransferOwnership(this.destination);
+
+            // Verify that PersistentState was called to update the contract owner
+            this.mockPersistentState.Verify(s => s.SetAddress($"Owner", this.destination));
+
+            this.mockContractLogger.Verify(l => l.Log(It.IsAny<ISmartContractState>(), new InterFluxStandardToken.OwnershipTransferred() { PreviousOwner = this.owner, NewOwner = this.destination }));
+        }
+
+        [Fact]
+        public void TransferOwnership_Fails_For_NonOwner()
+        {
+            UInt256 totalSupply = 100_000;
+            this.mockContractState.Setup(m => m.Message).Returns(new Message(this.contract, this.owner, 0));
+
+            var standardToken = new InterFluxStandardToken(this.mockContractState.Object, totalSupply, this.name, this.symbol, this.decimals, this.nativeChain, this.nativeAddress);
+
+            // Setup the owner of the contract
+            this.mockPersistentState.Setup(s => s.GetAddress($"Owner")).Returns(this.owner);
+
+            this.mockContractState.Setup(m => m.Message).Returns(new Message(this.contract, this.sender, 0));
+
+            Assert.ThrowsAny<SmartContractAssertException>(() => standardToken.TransferOwnership(this.destination));
+        }
+
+        [Fact]
+        public void Mint_Increases_Balance_And_TotalSupply()
+        {
+            UInt256 balance = 100;
+            UInt256 mintAmount = 20;
+
+            this.mockContractState.Setup(m => m.Message).Returns(new Message(this.contract, this.sender, 0));
+
+            var standardToken = new InterFluxStandardToken(this.mockContractState.Object, 100_000, this.name, this.symbol, this.decimals, this.nativeChain, this.nativeAddress);
+
+            // Setup the total supply
+            this.mockPersistentState.Setup(s => s.GetUInt256($"TotalSupply")).Returns(100_000);
+
+            // Setup the owner of the contract; without this the mint will fail
+            this.mockPersistentState.Setup(s => s.GetAddress($"Owner")).Returns(this.sender);
+
+            // Setup the balance of the sender's address in persistent state
+            this.mockPersistentState.Setup(s => s.GetUInt256($"Balance:{this.sender}")).Returns(balance);
+
+            standardToken.Mint(this.sender, mintAmount);
+
+            this.mockContractLogger.Verify(l => l.Log(It.IsAny<ISmartContractState>(), new InterFluxStandardToken.TransferLog { From = Address.Zero, To = this.sender, Amount = mintAmount }));
+
+            // Verify we queried the balance
+            this.mockPersistentState.Verify(s => s.GetUInt256($"Balance:{this.sender}"));
+
+            // Verify we set the sender's balance
+            this.mockPersistentState.Verify(s => s.SetUInt256($"Balance:{this.sender}", balance + mintAmount));
+
+            // Verify that the total supply was increased
+            this.mockPersistentState.Verify(s => s.SetUInt256($"TotalSupply", 100_000 + mintAmount));
+        }
+
+        [Fact]
+        public void Mint_Fails_For_NonOwner()
+        {
+            UInt256 balance = 100;
+            UInt256 mintAmount = 20;
+
+            this.mockContractState.Setup(m => m.Message).Returns(new Message(this.contract, this.owner, 0));
+
+            var standardToken = new InterFluxStandardToken(this.mockContractState.Object, 100_000, this.name, this.symbol, this.decimals, this.nativeChain, this.nativeAddress);
+
+            // Setup the owner of the contract
+            this.mockPersistentState.Setup(s => s.GetAddress($"Owner")).Returns(this.owner);
+
+            // Attempt the mint from a different address
+            this.mockContractState.Setup(m => m.Message).Returns(new Message(this.contract, this.sender, 0));
+
+            Assert.ThrowsAny<SmartContractAssertException>(() => standardToken.Mint(this.sender, mintAmount));
+        }
+
+        [Fact]
+        public void Burn_Decreases_Balance_And_TotalSupply()
+        {
+            UInt256 balance = 100;
+            UInt256 burnAmount = 20;
+
+            this.mockContractState.Setup(m => m.Message).Returns(new Message(this.contract, this.sender, 0));
+
+            var standardToken = new InterFluxStandardToken(this.mockContractState.Object, 100_000, this.name, this.symbol, this.decimals, this.nativeChain, this.nativeAddress);
+
+            // Setup the total supply
+            this.mockPersistentState.Setup(s => s.GetUInt256($"TotalSupply")).Returns(100_000);
+
+            // Setup the balance of the sender's address in persistent state
+            this.mockPersistentState.Setup(s => s.GetUInt256($"Balance:{this.sender}")).Returns(balance);
+
+            // Setup the balance of the recipient's address in persistent state
+            this.mockPersistentState.Setup(s => s.GetUInt256($"Balance:{Address.Zero}")).Returns(0);
+
+            standardToken.Burn(burnAmount);
+
+            this.mockContractLogger.Verify(l => l.Log(It.IsAny<ISmartContractState>(), new InterFluxStandardToken.TransferLog { From = this.sender, To = Address.Zero, Amount = burnAmount }));
+
+            // Verify we queried the balance
+            this.mockPersistentState.Verify(s => s.GetUInt256($"Balance:{this.sender}"));
+
+            // Verify we set the sender's balance
+            this.mockPersistentState.Verify(s => s.SetUInt256($"Balance:{this.sender}", balance - burnAmount));
+
+            // Verify we set the receiver's balance (i.e. the zero address)
+            this.mockPersistentState.Verify(s => s.SetUInt256($"Balance:{Address.Zero}", burnAmount));
+
+            // Verify that the total supply was decreased
+            this.mockPersistentState.Verify(s => s.SetUInt256($"TotalSupply", 100_000 - burnAmount));
+        }
+
+        [Fact]
+        public void Burn_For_Amount_Exceeding_Balance_Fails()
+        {
+            UInt256 balance = 100;
+            UInt256 burnAmount = 120;
+
+            this.mockContractState.Setup(m => m.Message).Returns(new Message(this.contract, this.sender, 0));
+
+            var standardToken = new InterFluxStandardToken(this.mockContractState.Object, 100_000, this.name, this.symbol, this.decimals, this.nativeChain, this.nativeAddress);
+
+            // Setup the total supply
+            this.mockPersistentState.Setup(s => s.GetUInt256($"TotalSupply")).Returns(100_000);
+
+            // Setup the balance of the sender's address in persistent state
+            this.mockPersistentState.Setup(s => s.GetUInt256($"Balance:{this.sender}")).Returns(balance);
+
+            // Setup the balance of the recipient's address in persistent state
+            this.mockPersistentState.Setup(s => s.GetUInt256($"Balance:{Address.Zero}")).Returns(0);
+
+            Assert.False(standardToken.Burn(burnAmount));
+        }
+
+        [Fact]
+        public void BurnWithMetadata_Records_Metadata_In_Log()
+        {
+            UInt256 balance = 100;
+            UInt256 burnAmount = 20;
+
+            this.mockContractState.Setup(m => m.Message).Returns(new Message(this.contract, this.sender, 0));
+
+            var standardToken = new InterFluxStandardToken(this.mockContractState.Object, 100_000, this.name, this.symbol, this.decimals, this.nativeChain, this.nativeAddress);
+
+            // Setup the total supply
+            this.mockPersistentState.Setup(s => s.GetUInt256($"TotalSupply")).Returns(100_000);
+
+            // Setup the balance of the sender's address in persistent state
+            this.mockPersistentState.Setup(s => s.GetUInt256($"Balance:{this.sender}")).Returns(balance);
+
+            // Setup the balance of the recipient's address in persistent state
+            this.mockPersistentState.Setup(s => s.GetUInt256($"Balance:{Address.Zero}")).Returns(0);
+
+            standardToken.BurnWithMetadata(burnAmount, "Hello world");
+
+            this.mockContractLogger.Verify(l => l.Log(It.IsAny<ISmartContractState>(), new InterFluxStandardToken.TransferLog { From = this.sender, To = Address.Zero, Amount = burnAmount }));
+
+            // Verify we queried the balance
+            this.mockPersistentState.Verify(s => s.GetUInt256($"Balance:{this.sender}"));
+
+            // Verify we set the sender's balance
+            this.mockPersistentState.Verify(s => s.SetUInt256($"Balance:{this.sender}", balance - burnAmount));
+
+            // Verify we set the receiver's balance (i.e. the zero address)
+            this.mockPersistentState.Verify(s => s.SetUInt256($"Balance:{Address.Zero}", burnAmount));
+
+            // Verify that the total supply was decreased
+            this.mockPersistentState.Verify(s => s.SetUInt256($"TotalSupply", 100_000 - burnAmount));
+
+            this.mockContractLogger.Verify(l => l.Log(It.IsAny<ISmartContractState>(), new InterFluxStandardToken.BurnMetadata() { From = this.sender, Amount = burnAmount, Metadata = "Hello world" }));
+        }
+    }
+}
