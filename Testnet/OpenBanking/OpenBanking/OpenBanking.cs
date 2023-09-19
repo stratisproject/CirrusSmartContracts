2cf00683965bf245ae37d0a84fd7ca16c3486eb8
quantumagi <someguy.fromafrica@gmail.com>
2023-09-19 16:19:41 +1000

    OpenBanking contract

---
+++ Testnet/OpenBanking/OpenBanking/OpenBanking.cs
@@ -0,0 +1,83 @@
+﻿using Stratis.SmartContracts;
+
+[Deploy]
+public class OpenBanking : SmartContract
+{
+    public OpenBanking(ISmartContractState smartContractState) : base(smartContractState)
+    {
+        // Initialize your state variables if needed
+    }
+
+    public enum WithdrawalStatus { Requested, Pending, Executed, Failed }
+
+    public struct Withdrawal
+    {
+        public UInt256 Amount;
+        public Address To;
+        public WithdrawalStatus Status;
+    }
+
+    private void SetWithdrawal(string id, Withdrawal withdrawal)
+    {
+        State.SetStruct($"Withdrawal:{id}", withdrawal);
+    }
+
+    private Withdrawal GetWithdrawal(string id)
+    {
+        return State.GetStruct<Withdrawal>($"Withdrawal:{id}");
+    }
+
+    public void RequestWithdrawal(string withdrawalID, UInt256 amount, Address to)
+    {
+        Assert(Message.Sender == /*Your multisig contract address*/);
+        var withdrawal = new Withdrawal() { Amount = amount, To = to, Status = WithdrawalStatus.Requested };
+        SetWithdrawal(withdrawalID, withdrawal);
+        Log(new WithdrawalRequestedLog { WithdrawalID = withdrawalID, Amount = amount, To = to });
+    }
+
+    public Withdrawal[] GetRequestedWithdrawals()
+    {
+        // Implement your logic to return requested withdrawals
+        return new Withdrawal[0]; // Placeholder
+    }
+
+    public void MarkAsPending(string withdrawalID)
+    {
+        Assert(Message.Sender == /*Your Web Demo UI address*/);
+        var withdrawal = GetWithdrawal(withdrawalID);
+        withdrawal.Status = WithdrawalStatus.Pending;
+        SetWithdrawal(withdrawalID, withdrawal);
+        Log(new WithdrawalStatusChangedLog { WithdrawalID = withdrawalID, NewStatus = WithdrawalStatus.Pending });
+    }
+
+    public void MarkAsExecuted(string withdrawalID)
+    {
+        Assert(Message.Sender == /*Your multisig contract address*/);
+        var withdrawal = GetWithdrawal(withdrawalID);
+        withdrawal.Status = WithdrawalStatus.Executed;
+        SetWithdrawal(withdrawalID, withdrawal);
+        // Logic for burning tokens can go here
+        Log(new WithdrawalStatusChangedLog { WithdrawalID = withdrawalID, NewStatus = WithdrawalStatus.Executed });
+    }
+
+    public void MarkAsFailed(string withdrawalID)
+    {
+        var withdrawal = GetWithdrawal(withdrawalID);
+        withdrawal.Status = WithdrawalStatus.Failed;
+        SetWithdrawal(withdrawalID, withdrawal);
+        Log(new WithdrawalStatusChangedLog { WithdrawalID = withdrawalID, NewStatus = WithdrawalStatus.Failed });
+    }
+
+    public struct WithdrawalRequestedLog
+    {
+        public string WithdrawalID;
+        public UInt256 Amount;
+        public Address To;
+    }
+
+    public struct WithdrawalStatusChangedLog
+    {
+        public string WithdrawalID;
+        public WithdrawalStatus NewStatus;
+    }
+}
